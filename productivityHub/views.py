61f22a81bdf10581e3899a5789fd3650d7706d13
Codeict <156934036+Nick24d@users.noreply.github.com>
2025-01-08 22:16:57 +0000

    new commit

--- productivityHub/views.py
+++ productivityHub/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+
+# sign up view
+def signup(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      login(request, form.instance)
+      return redirect('login')
+
 
 # Create your views here.
